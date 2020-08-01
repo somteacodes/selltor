3887c5e909fbfff64f85e834ff4fe17957c2ac88
somteacodes <somteacodes@gmail.com>
2020-08-01 22:21:44 +0100

    navbar and home search design

---
+++ build/includes/navbar.php
@@ -0,0 +1,22 @@
+<div class="bg-primary px-4 lg:px-16">
+    <div class="grid grid-cols-2 py-8">
+        <img src="img/logo_big.svg" alt="" class="w-32">
+        <div class="flex justify-end">
+            <button class="bg-secondary hover:bg-secondary-hover rounded px-4 py-2 text-white hidden lg:inline">post
+                a
+                shelter</button>
+
+            <div class="lg:flex items-center text-white ml-4">
+                <img src="img/user_icon.svg" alt="">
+
+                <p>Sign in / Register</p>
+            </div>
+
+
+
+            <a href="#" class="lg:hidden">
+                <img src="img/hamburger_menu.svg" alt="">
+            </a>
+        </div>
+    </div>
+</div>
---
+++ build/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selltor: Buy and sell shelters</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+
+<body>
+    <!-- navbar -->
+    <?php include 'includes/navbar.php';?>
+    <!-- navbar ends -->
+
+    <!-- home search -->
+    <?php include 'includes/home_search.php';?>
+    <!-- home search ends -->
+
+    <div class="text-center bg-gray-300">
+
+        <div class="w-full flex items-center justify-center relative" style=top:-2rem>
+
+            <span class="rounded-full h-16 w-16 bg-gray-400 text-secondary flex items-center justify-center">
+                V
+            </span>
+        </div>
+
+        <div class="divide-y-2 divide-secondary-hover w-auto pb-10 text-gray-700 inline-block text-center">
+            <p class="text-4xl font-bold ">Explore Properties</p>
+            <p class="text-sm">Explore properties from top ranking agents.</p>
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
