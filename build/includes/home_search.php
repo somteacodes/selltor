9cfd612a31dba05b437218bf0f7deacd044c8f5b
somteacodes <somteacodes@gmail.com>
2020-08-01 22:03:59 +0100

    first commit

---
+++ build/includes/home_search.php
@@ -0,0 +1,59 @@
+<div class="py-20 bg-cover bg-no-repeat bg-right lg:px-16" style="background-image:url(img/header_img.png);">
+    <div class="text-white text-center mb-10">
+        <p class="font-bold text-4xl">Best prices of properties</p>
+        <p class="font-bold text-sm">Your one stop for all properties search</p>
+    </div>
+
+    <div class="text-center w-full">
+        <form action="" class="lg:inline-block w-auto bg-white bg-opacity-25 p-2 rounded">
+
+            <div class="lg:flex items-center bg-white px-4 py-2 rounded">
+
+
+                <span class="flex p-3">
+                    <img src="img/house_icon.svg" alt="">
+
+                    <input type="text" class="placeholder-gray-700 py-2 px-4 focus:outline-none"
+                        placeholder="What Shelter are you looking for?">
+                </span>
+
+                <span class="hidden lg:inline border-gray-500 border-r-2 mr-5"> &nbsp;</span>
+
+
+                <span class="flex p-3">
+                    <img src="img/location_icon.svg" alt="">
+
+                    <input type="text" class="placeholder-gray-700 py-2 px-4 focus:outline-none" placeholder="Location">
+                </span>
+
+                <span class="hidden lg:inline border-gray-500 border-r-2 mr-5"> &nbsp;</span>
+
+                <span class="flex p-3">
+                    <img src="img/sale_icon.svg" alt="">
+                    <div class="text-gray-500 p-2">
+                        <span>For Sale</span>
+                        <label class="switch mx-1">
+                            <input type="checkbox" name="status" value="Rent">
+                            <span class="slider round"></span>
+                        </label>
+                        <span>For Rent</span>
+                    </div>
+
+                </span>
+                <span class="flex p-3">
+                    <button type="submit"
+                        class="bg-secondary hover:bg-secondary-hover text-white rounded flex py-2 px-4 w-full lg:w-auto justify-center">
+                        Search
+                        <img src="img/search_icon.svg" alt="" class="pl-5">
+
+                    </button>
+
+                </span>
+
+
+            </div>
+        </form>
+    </div>
+
+
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#14213D',
+        'secondary': '#567AC8',
+        'accent': '#FCA311',
+        'secondary-hover': '#385AA4'
+      },
+
+    },
+  },
+  variants: {},
+  plugins: [],
+}
